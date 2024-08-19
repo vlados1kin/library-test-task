d3457c07df4d1460c44c902bbdf98b48f81e90ae
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 18:54:01 +0300

    add repository context factory

---
+++ Library.API/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Library.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Library.API.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsettings.json");
+        var connection = builder.Build().GetConnectionString("sqlConnection");
+        var options = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(connection, o => o.MigrationsAssembly("Library.API"))
+            .Options;
+        return new RepositoryContext(options);
+    }
+}
