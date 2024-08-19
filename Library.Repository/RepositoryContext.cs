2ff2e094b646135ee4d77781dd8d68e0a167fd5c
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 18:46:47 +0300

    add repository context

---
+++ Library.Repository/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Library.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Author> Authors = null!;
+    public DbSet<Book> Books = null!;
+    public DbSet<Genre> Genres = null!;
+}
