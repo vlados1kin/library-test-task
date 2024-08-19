29b6907c98129bc8fc06c1eae08883d65d7de9df
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 19:24:35 +0300

    fix property type

--- Library.Domain/Models/Book.cs
+++ Library.Domain/Models/Book.cs
@@ -5,7 +5,7 @@ public class Book
     public Guid Id { get; set; }
     public string ISBN { get; set; }
     public string Name { get; set; }
-    public Guid GenreId { get; set; }
+    public Guid? GenreId { get; set; }
     public string? Title { get; set; }
     public Guid AuthorId { get; set; }
     public DateTime ReceiveTime { get; set; }
