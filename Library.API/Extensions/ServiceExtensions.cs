75ea567ad628fed266b0c55aafc7da3950e9d8db
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 19:00:13 +0300

    fix configure db context method

--- Library.API/Extensions/ServiceExtensions.cs
+++ Library.API/Extensions/ServiceExtensions.cs
@@ -5,7 +5,7 @@ namespace Library.API.Extensions;
 
 public static class ServiceExtensions
 {
-    public static void ConfigureDbContext(IServiceCollection services, IConfiguration configuration)
+    public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
         => services.AddDbContext<RepositoryContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 }
