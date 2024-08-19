df46c68dc8e67c8e426314ebda501613b7b54a65
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 22:30:03 +0300

    create author dtos

---
+++ Library.Shared/DTO/AuthorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Shared.DTO;
+
+public record AuthorDto
+{
+    public Guid Id { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public DateOnly Birthday { get; init; }
+    public string Country { get; init; }
+}
---
+++ Library.Shared/DTO/AuthorForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.Shared.DTO;
+
+public record AuthorForUpdateDto
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public DateOnly Birthday { get; init; }
+    public string Country { get; init; }
+}
