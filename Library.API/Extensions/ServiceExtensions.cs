d993c1e47b153c661e7c4bfc0077bd0c8848d65f
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 18:58:12 +0300

    create service extensions class

---
+++ Library.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Library.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureDbContext(IServiceCollection services, IConfiguration configuration)
+        => services.AddDbContext<RepositoryContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+}
