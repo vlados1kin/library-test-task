784a2d6ef3aba6e4a62a859d000b98a43ad7db70
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 22:52:31 +0300

    add constructor to author service

--- Library.Service/AuthorService.cs
+++ Library.Service/AuthorService.cs
@@ -1,10 +1,20 @@
-﻿using Library.Contracts;
+﻿using AutoMapper;
+using Library.Contracts;
 using Library.Shared.DTO;
 
 namespace Library.Service;
 
 public class AuthorService : IAuthorService
 {
+    private readonly IRepositoryManager _repository;
+    private readonly IMapper _mapper;
+
+    public AuthorService(IRepositoryManager repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+    
     public async Task<IEnumerable<AuthorDto>> GetAuthorsAsync(bool trackChanges)
     {
         throw new NotImplementedException();
