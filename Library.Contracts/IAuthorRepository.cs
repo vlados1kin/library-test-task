66b04aa22efd460c5cc083bf04ddc589a4d9a370
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 19:49:32 +0300

    create author repository interface

---
+++ Library.Contracts/IAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Library.Domain.Models;
+
+namespace Library.Contracts;
+
+public interface IAuthorRepository
+{
+    Task<IEnumerable<Author>> GetAuthorsAsync(bool trackChanges);
+    Task<Author> GetAuthorByIdAsync(bool trackChanges);
+    void CreateAuthor(Author author);
+    void DeleteAuthor(Guid id);
+}
