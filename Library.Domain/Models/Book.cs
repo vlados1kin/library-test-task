4c618b4745523910cc95de63aac08bcb31981666
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 19:22:02 +0300

    change property types

--- Library.Domain/Models/Book.cs
+++ Library.Domain/Models/Book.cs
@@ -6,11 +6,11 @@ public class Book
     public string ISBN { get; set; }
     public string Name { get; set; }
     public Guid GenreId { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public Guid AuthorId { get; set; }
     public DateTime ReceiveTime { get; set; }
     public DateTime ReturnTime { get; set; }
     
-    public Genre Genre { get; set; }
+    public Genre? Genre { get; set; }
     public Author Author { get; set; }
 }
