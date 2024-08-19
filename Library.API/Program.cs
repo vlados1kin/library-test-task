77cdea143f4fc41f988a0ee13eaa56e45793fc53
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 19:00:29 +0300

    add context service in service collection

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -1,9 +1,13 @@
+using Library.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureDbContext(builder.Configuration);
 
 var app = builder.Build();
 
