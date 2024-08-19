901e5ef8bd0fa882c2d68316a057fe732363464e
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-19 22:29:28 +0300

    create author service

---
+++ Library.Contracts/IAuthorService.cs
@@ -0,0 +1,12 @@
+﻿using Library.Shared.DTO;
+
+namespace Library.Contracts;
+
+public interface IAuthorService
+{
+    Task<IEnumerable<AuthorDto>> GetAuthorsAsync(bool trackChanges);
+    Task<AuthorDto> GetAuthorById(Guid id, bool trackChanges);
+    Task CreateAuthorAsync(AuthorForCreationDto authorForCreationDto);
+    Task UpdateAuthorAsync(Guid id, AuthorForUpdateDto authorForUpdateDto, bool trackChanges);
+    Task DeleteAuthorAsync(Guid id, bool trackChanges);
+}
---
+++ Library.Service/AuthorService.cs
@@ -0,0 +1,32 @@
+﻿using Library.Contracts;
+using Library.Shared.DTO;
+
+namespace Library.Service;
+
+public class AuthorService : IAuthorService
+{
+    public async Task<IEnumerable<AuthorDto>> GetAuthorsAsync(bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<AuthorDto> GetAuthorById(Guid id, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task CreateAuthorAsync(AuthorForCreationDto authorForCreationDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAuthorAsync(Guid id, AuthorForUpdateDto authorForUpdateDto, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAuthorAsync(Guid id, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+}
