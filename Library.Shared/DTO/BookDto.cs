ecad2e12407f6e4895ed6561d87fe4ddf2ff84ce
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-20 13:56:20 +0300

    fix book dto

--- Library.Shared/DTO/BookDto.cs
+++ Library.Shared/DTO/BookDto.cs
@@ -2,10 +2,9 @@
 
 public record BookDto
 {
-    public Guid Id { get; init; }
     public string ISBN { get; init; }
     public string Name { get; init; }
-    public Guid? GenreId { get; init; }
+    public string Genre { get; init; }
     public string? Title { get; init; }
     public Guid AuthorId { get; init; }
     public DateTime ReceiveTime { get; init; }
