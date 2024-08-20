9e793aff5faa5c2f8024f31966145ca09a0e9eaa
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-20 08:08:14 +0300

    create new exception class

---
+++ Library.Domain/Exceptions/AuthorNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Domain.Exceptions;
+
+public class AuthorNotFoundException : NotFoundException
+{
+    public AuthorNotFoundException(Guid id) : base($"The author with id: {id} does not exist in the database.")
+    {
+    }
+}
