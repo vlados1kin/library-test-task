b1f7559f5808545c71b9340a15e4f471a73bf34b
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-20 15:59:48 +0300

    create new book dtos

---
+++ Library.Shared/DTO/BookForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Shared.DTO;
+
+public record BookForCreationDto
+{
+    public string ISBN { get; set; }
+    public string Name { get; set; }
+    public Guid? GenreId { get; set; }
+    public string? Title { get; set; }
+    public Guid AuthorId { get; set; }
+    public DateTime ReceiveTime { get; set; }
+    public DateTime ReturnTime { get; set; }
+}
---
+++ Library.Shared/DTO/BookForIssueDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Library.Shared.DTO;
+
+public record BookForIssueDto
+{
+    public DateTime ReceiveTime { get; set; }
+    public DateTime ReturnTime { get; set; }
+}
