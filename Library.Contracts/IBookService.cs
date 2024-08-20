08d572a051b28a48f5fb8f2af945ccdea2472689
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-20 16:00:33 +0300

    create and implement a book interface

---
+++ Library.Contracts/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using Library.Shared.DTO;
+
+namespace Library.Contracts;
+
+public interface IBookService
+{
+    Task<IEnumerable<BookDto>> GetBooksAsync(bool trackChanges);
+    Task<BookDto> GetBookByIdAsync(Guid id, bool trackChanges);
+    Task<BookDto> GetBookByIsbnAsync(string isbn, bool trackChanges);
+    Task<BookDto> CreateBookAsync(BookForCreationDto bookForCreationDto);
+    Task UpdateBookAsync(Guid id, BookForUpdateDto bookForUpdateDto, bool trackChanges);
+    Task DeleteBookAsync(Guid id, bool trackChanges);
+    Task IssueBooksToUser(Guid id, BookForIssueDto bookForIssueDto, bool trackChanges);
+    // TODO: добавление изображения к книге
+    // TODO: отправка уведомления об истечении срока выдачи книги
+}
---
+++ Library.Service/BookService.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Library.Contracts;
+using Library.Domain.Exceptions;
+using Library.Domain.Models;
+using Library.Shared.DTO;
+
+namespace Library.Service;
+
+public class BookService : IBookService
+{
+    private readonly IRepositoryManager _repository;
+    private readonly IMapper _mapper;
+    
+    public BookService(IRepositoryManager repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<BookDto>> GetBooksAsync(bool trackChanges)
+    {
+        var book = await _repository.Book.GetBooksAsync(trackChanges);
+        var bookDto = _mapper.Map<IEnumerable<BookDto>>(book);
+        return bookDto;
+    }
+
+    public async Task<BookDto> GetBookByIdAsync(Guid id, bool trackChanges)
+    {
+        var book = await _repository.Book.GetBookByIdAsync(id, trackChanges);
+        if (book is null)
+            throw new BookWithIdNotFoundException(id);
+        var bookDto = _mapper.Map<BookDto>(book);
+        return bookDto;
+    }
+
+    public async Task<BookDto> GetBookByIsbnAsync(string isbn, bool trackChanges)
+    {
+        var book = await _repository.Book.GetBookByIsbnAsync(isbn, trackChanges);
+        if (book is null)
+            throw new BookWithIsbnNotFoundException(isbn);
+        var bookDto = _mapper.Map<BookDto>(book);
+        return bookDto;
+    }
+
+    public async Task<BookDto> CreateBookAsync(BookForCreationDto bookForCreationDto)
+    {
+        var book = _mapper.Map<Book>(bookForCreationDto);
+        _repository.Book.CreateBook(book);
+        await _repository.SaveAsync();
+        var bookDto = _mapper.Map<BookDto>(book);
+        return bookDto;
+    }
+
+    public async Task UpdateBookAsync(Guid id, BookForUpdateDto bookForUpdateDto, bool trackChanges)
+    {
+        var book = await _repository.Book.GetBookByIdAsync(id, trackChanges);
+        if (book is null)
+            throw new BookWithIdNotFoundException(id);
+        _mapper.Map(bookForUpdateDto, book);
+        await _repository.SaveAsync();
+    }
+
+    public async Task DeleteBookAsync(Guid id, bool trackChanges)
+    {
+        var book = await _repository.Book.GetBookByIdAsync(id, trackChanges);
+        if (book is null)
+            throw new BookWithIdNotFoundException(id);
+        _repository.Book.DeleteBook(book);
+        await _repository.SaveAsync();
+    }
+
+    public async Task IssueBooksToUser(Guid id, BookForIssueDto bookForIssueDto, bool trackChanges)
+    {
+        var book = await _repository.Book.GetBookByIdAsync(id, trackChanges);
+        if (book is null)
+            throw new BookWithIdNotFoundException(id);
+        _mapper.Map(bookForIssueDto, book);
+        await _repository.SaveAsync();
+    }
+}
