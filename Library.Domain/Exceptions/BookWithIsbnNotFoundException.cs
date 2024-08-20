5d49e71fefe29e83d50adec7ec70d51b27d7dc85
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-20 16:00:08 +0300

    create new not found exceptions for book

---
+++ Library.Domain/Exceptions/BookWithIsbnNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Domain.Exceptions;
+
+public class BookWithIsbnNotFoundException : NotFoundException
+{
+    public BookWithIsbnNotFoundException(string isbn) : base($"The book with ISBN: {isbn} does not exist in the database.")
+    {
+    }
+}
