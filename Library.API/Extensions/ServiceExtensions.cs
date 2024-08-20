6c4f7537c9781d876b35dcd643fe6c8d28804f41
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-20 12:41:38 +0300

    configure cors and swagger

--- Library.API/Extensions/ServiceExtensions.cs
+++ Library.API/Extensions/ServiceExtensions.cs
@@ -2,18 +2,48 @@
 using Library.Repository;
 using Library.Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace Library.API.Extensions;
 
 public static class ServiceExtensions
 {
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithExposedHeaders("X-Pagination"));
+        });
+
     public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
         => services.AddDbContext<RepositoryContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 
     public static void ConfigureRepositoryManager(this IServiceCollection services)
         => services.AddScoped<IRepositoryManager, RepositoryManager>();
-    
+
     public static void ConfigureServiceManager(this IServiceCollection services)
         => services.AddScoped<IServiceManager, ServiceManager>();
+
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Library API", 
+                Version = "v1",
+                Description = "Library API for a traineeship",
+                Contact = new OpenApiContact
+                {
+                    Name = "Vladislav Lashkin",
+                    Email = "lashkin2004@gmail.com",
+                    Url = new Uri("https://t.me/vlados1kin"),
+                },
+            });
+        });
+    }
 }
