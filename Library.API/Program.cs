982ebf5860d835ea5b4cda1ded4a2eff038e4e41
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-20 12:42:29 +0300

    apply settings

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -4,27 +4,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.ConfigureSwagger();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureCors();
 builder.Services.ConfigureDbContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ConfigureServiceManager();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.ConfigureExceptionHandler();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseHsts();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(s => { s.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API v1"); });
 }
 
+app.ConfigureExceptionHandler();
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
