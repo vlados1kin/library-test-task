a0d9fa3ae4eefc5589eaff04f9dee96accbb446b
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-23 23:10:43 +0300

    fix user configuration

--- Library.Repository/Configurations/UserConfiguration.cs
+++ Library.Repository/Configurations/UserConfiguration.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Library.Repository.Configurations;
 
-public class UserConfiguration : IEntityTypeConfiguration<IdentityUser>
+public class UserConfiguration : IEntityTypeConfiguration<IdentityRole>
 {
-    public void Configure(EntityTypeBuilder<IdentityUser> builder)
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
     {
         builder.HasData(
             new IdentityRole()
