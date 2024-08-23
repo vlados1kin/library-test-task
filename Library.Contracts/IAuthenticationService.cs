a30d01a40e56093985d0160cad6987b6f089cda1
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-23 15:45:26 +0300

    create auth service

---
+++ Library.Contracts/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using Library.Shared.DTO;
+using Microsoft.AspNetCore.Identity;
+
+namespace Library.Contracts;
+
+public interface IAuthenticationService
+{
+    Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto);
+}
---
+++ Library.Service/AuthenticationService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Library.Contracts;
+using Library.Domain.Models;
+using Library.Shared.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace Library.Service;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IMapper _mapper;
+    private readonly UserManager<User> _userManager;
+    private readonly IConfiguration _configuration;
+    
+    public AuthenticationService(IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+    {
+        _mapper = mapper;
+        _userManager = userManager;
+        _configuration = configuration;
+    }
+    
+    public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+    {
+        var user = _mapper.Map<User>(userForRegistration);
+        var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+        if (result.Succeeded)
+            await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+        return result;
+    }
+}
