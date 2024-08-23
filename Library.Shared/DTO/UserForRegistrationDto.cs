a518239ea40378335cf267fe77eb7b892bafb19a
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-23 12:43:08 +0300

    create user for registration dto

---
+++ Library.Shared/DTO/UserForRegistrationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Shared.DTO;
+
+public record UserForRegistrationDto
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    [Required(ErrorMessage = "Username is required.")]
+    public string UserName { get; init; }
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; init; }
+    public ICollection<string>? Roles { get; init; }
+}
