a9e6f16c14b9ad2f9781c3e00a842299be9a2234
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-23 15:45:37 +0300

    create auth controller

---
+++ Library.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using Library.Contracts;
+using Library.Shared.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Presentation.Controllers;
+
+[ApiController]
+[Route("api/authentication")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public AuthenticationController(IServiceManager service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistrationDto)
+    {
+        var result = await _service.AuthenticationService.RegisterUser(userForRegistrationDto);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+        return StatusCode(201);
+    }
+}
