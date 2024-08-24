a1e78062124b894622d785b21cfdb4b31f3e0de6
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 23:06:53 +0300

    create user dto

---
+++ Library.Shared/DTO/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Shared.DTO;
+
+public record UserDto
+{
+    public Guid Id { get; init; }
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public string? UserName { get; init; }
+    public string? PasswordHash { get; init; }
+}
