b0882ee2825a1742498e29c1488d13a99d4b149c
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 17:22:21 +0300

    create genre exception

---
+++ Library.Domain/Exceptions/GenreNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Domain.Exceptions;
+
+public class GenreNotFoundException : NotFoundException
+{
+    public GenreNotFoundException(Guid id) : base($"The genre with id: {id} does not exist in the database.")
+    {
+    }
+}
