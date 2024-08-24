7df225c6273286604b29e52d73cfdfb83fa3e6ed
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 22:02:08 +0300

    fix book domain model

--- Library.Domain/Models/Book.cs
+++ Library.Domain/Models/Book.cs
@@ -8,9 +8,8 @@ public class Book
     public Guid? GenreId { get; set; }
     public string? Title { get; set; }
     public Guid AuthorId { get; set; }
-    public DateTime ReceiveTime { get; set; }
-    public DateTime ReturnTime { get; set; }
     
     public Genre? Genre { get; set; }
     public Author Author { get; set; }
+    public Issue? Issue { get; set; }
 }
