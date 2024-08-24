54a93204a3678e1ab0cdfc6060cd23aefbacca84
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 22:01:33 +0300

    create configuration for issue model

---
+++ Library.Repository/Configurations/IssueConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Library.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Repository.Configurations;
+
+public class IssueConfiguration : IEntityTypeConfiguration<Issue>
+{
+    public void Configure(EntityTypeBuilder<Issue> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.HasOne(i => i.Book)
+            .WithOne(b => b.Issue)
+            .HasForeignKey<Issue>(i => i.BookId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(i => i.User)
+            .WithMany(u => u.Issues)
+            .HasForeignKey(i => i.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(i => i.ReceiveTime).IsRequired();
+
+        builder.Property(i => i.ReturnTime).IsRequired();
+    }
+}
