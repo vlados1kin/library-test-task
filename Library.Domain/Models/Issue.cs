c9159641c6f1484c2aa433f115e0f462fd6bf65b
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 22:01:19 +0300

    create issue domain model

---
+++ Library.Domain/Models/Issue.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Domain.Models;
+
+public class Issue
+{
+    public Guid Id { get; set; }
+    public Guid BookId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime ReceiveTime { get; set; }
+    public DateTime ReturnTime { get; set; }
+    
+    public Book Book { get; set; }
+    public User User { get; set; }
+}
