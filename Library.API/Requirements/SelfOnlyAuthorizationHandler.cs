e7c9c26f6c01e5f774a085e36c28ded515e35660
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-25 01:32:43 +0300

    configure policy

---
+++ Library.API/Requirements/SelfOnlyAuthorizationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Library.API.Requirements;
+
+public class SelfOnlyAuthorizationHandler : AuthorizationHandler<SelfOnlyAuthorizationRequirement>
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public SelfOnlyAuthorizationHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, SelfOnlyAuthorizationRequirement requirement)
+    {
+        var userRole = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+        if (userRole.Equals("Admin", StringComparison.InvariantCultureIgnoreCase))
+            context.Succeed(requirement);
+        
+        var userId = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        var resourceId = _httpContextAccessor.HttpContext?.Request.RouteValues["id"]?.ToString();
+
+        if (userId.Equals(resourceId, StringComparison.InvariantCultureIgnoreCase))
+            context.Succeed(requirement);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Library.API/Requirements/SelfOnlyAuthorizationRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Library.API.Requirements;
+
+public class SelfOnlyAuthorizationRequirement : IAuthorizationRequirement
+{
+    
+}
