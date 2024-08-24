aa1d5dbde0113b4cdb613366f902f0120d7d6480
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 23:06:24 +0300

    create issue not found exception

---
+++ Library.Domain/Exceptions/IssueNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Domain.Exceptions;
+
+public class IssueNotFoundException : NotFoundException
+{
+    public IssueNotFoundException(Guid id) : base($"The issue with id: {id} does not exist in the database.")
+    {
+    }
+}
