0ef2a90fbd7f8418b4d274f033da574d8973b685
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 17:29:32 +0300

    include genre in repo and service manager

--- Library.Repository/RepositoryManager.cs
+++ Library.Repository/RepositoryManager.cs
@@ -7,15 +7,18 @@ public sealed class RepositoryManager : IRepositoryManager
     private readonly RepositoryContext _repositoryContext;
     private readonly Lazy<IAuthorRepository> _authorRepository;
     private readonly Lazy<IBookRepository> _bookRepository;
+    private readonly Lazy<IGenreRepository> _genreRepository;
 
     public RepositoryManager(RepositoryContext repositoryContext)
     {
         _repositoryContext = repositoryContext;
         _authorRepository = new Lazy<IAuthorRepository>(() => new AuthorRepository(repositoryContext));
         _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(repositoryContext));
+        _genreRepository = new Lazy<IGenreRepository>(() => new GenreRepository(repositoryContext));
     }
 
     public IAuthorRepository Author => _authorRepository.Value;
     public IBookRepository Book => _bookRepository.Value;
+    public IGenreRepository Genre => _genreRepository.Value;
     public async Task SaveAsync() => await _repositoryContext.SaveChangesAsync();
 }
--- Library.Service/ServiceManager.cs
+++ Library.Service/ServiceManager.cs
@@ -12,6 +12,7 @@ public class ServiceManager : IServiceManager
 {
     private readonly Lazy<IAuthorService> _authorService;
     private readonly Lazy<IBookService> _bookService;
+    private readonly Lazy<IGenreService> _genreService;
     private readonly Lazy<IImageService> _imageService;
     private readonly Lazy<IAuthenticationService> _authenticationService;
 
@@ -24,13 +25,14 @@ public class ServiceManager : IServiceManager
     {
         _authorService = new Lazy<IAuthorService>(() => new AuthorService(repository, mapper));
         _bookService = new Lazy<IBookService>(() => new BookService(repository, mapper));
+        _genreService = new Lazy<IGenreService>(() => new GenreService(repository, mapper));
         _imageService = new Lazy<IImageService>(() => new ImageService(options));
-        _authenticationService =
-            new Lazy<IAuthenticationService>(() => new AuthenticationService(mapper, userManager, configuration));
+        _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(mapper, userManager, configuration));
     }
 
     public IAuthorService AuthorService => _authorService.Value;
     public IBookService BookService => _bookService.Value;
+    public IGenreService GenreService => _genreService.Value;
     public IImageService ImageService => _imageService.Value;
     public IAuthenticationService AuthenticationService => _authenticationService.Value;
 }
