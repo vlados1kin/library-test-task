2d7591dd2d3e366c1154a981be7f055bb94b70ff
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-24 17:30:29 +0300

    create wrong image exception

---
+++ Library.Domain/Exceptions/WrongImageExtensionBadRequestException.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Domain.Exceptions;
+
+public class WrongImageExtensionBadRequestException : BadRequestException
+{
+    public WrongImageExtensionBadRequestException() : base("Wrong image extension. Required extension: .png")
+    {
+    }
+}
