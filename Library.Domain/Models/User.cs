d30ad5baf66bd7a8ae5439191d5a20fe4b82d201
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-25 18:59:29 +0300

    add refresh token property to user domain model

--- Library.Domain/Models/User.cs
+++ Library.Domain/Models/User.cs
@@ -6,6 +6,8 @@ public class User : IdentityUser<Guid>
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
 
     public ICollection<Issue>? Issues { get; set; }
 }
