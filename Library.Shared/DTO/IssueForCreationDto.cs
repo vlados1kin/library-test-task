12c5d84f449bc7f47f85994e38fe2b68b13692e3
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-25 18:34:14 +0300

    fix issue for creation dto

--- Library.Shared/DTO/IssueForCreationDto.cs
+++ Library.Shared/DTO/IssueForCreationDto.cs
@@ -2,6 +2,8 @@
 
 public record IssueForCreationDto
 {
+    public Guid UserId { get; init; }
+    public Guid BookId { get; init; }
     public DateTime ReceiveTime { get; init; }
     public DateTime ReturnTime { get; init; }
 }
