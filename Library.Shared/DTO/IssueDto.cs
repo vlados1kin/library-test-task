8788f6e7ff87554e76037868fd0f69982710c831
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-25 18:33:56 +0300

    fix issue dto

--- Library.Shared/DTO/IssueDto.cs
+++ Library.Shared/DTO/IssueDto.cs
@@ -1,10 +1,12 @@
-﻿namespace Library.Shared.DTO;
+﻿using Library.Domain.Models;
+
+namespace Library.Shared.DTO;
 
 public record IssueDto
 {
     public Guid Id { get; init; }
-    public Guid BookId { get; init; }
     public Guid UserId { get; init; }
+    public BookDto Book { get; init; }
     public DateTime ReceiveTime { get; init; }
     public DateTime ReturnTime { get; init; }
 }
