b6664f9d91e6b1997fd162522773e709ef7f4a76
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-02 18:20:25 +0300

    apply new configurations

---
+++ Library.API/Migrations/20240902120724_FixTableNames.cs
@@ -0,0 +1,258 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTableNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Genre_GenreId",
+                table: "Book");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issue_AspNetUsers_UserId",
+                table: "Issue");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issue_Book_BookId",
+                table: "Issue");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Issue",
+                table: "Issue");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Book",
+                table: "Book");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Author",
+                table: "Author");
+
+            migrationBuilder.RenameTable(
+                name: "Issue",
+                newName: "Issues");
+
+            migrationBuilder.RenameTable(
+                name: "Genre",
+                newName: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Book",
+                newName: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Author",
+                newName: "Authors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Issue_UserId",
+                table: "Issues",
+                newName: "IX_Issues_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Issue_BookId",
+                table: "Issues",
+                newName: "IX_Issues_BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Book_GenreId",
+                table: "Books",
+                newName: "IX_Books_GenreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Book_AuthorId",
+                table: "Books",
+                newName: "IX_Books_AuthorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Issues",
+                table: "Issues",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Genres_GenreId",
+                table: "Books",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_AspNetUsers_UserId",
+                table: "Issues",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issues_Books_BookId",
+                table: "Issues",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Genres_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_AspNetUsers_UserId",
+                table: "Issues");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Issues_Books_BookId",
+                table: "Issues");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Issues",
+                table: "Issues");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors");
+
+            migrationBuilder.RenameTable(
+                name: "Issues",
+                newName: "Issue");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "Genre");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "Book");
+
+            migrationBuilder.RenameTable(
+                name: "Authors",
+                newName: "Author");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Issues_UserId",
+                table: "Issue",
+                newName: "IX_Issue_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Issues_BookId",
+                table: "Issue",
+                newName: "IX_Issue_BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_GenreId",
+                table: "Book",
+                newName: "IX_Book_GenreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_AuthorId",
+                table: "Book",
+                newName: "IX_Book_AuthorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Issue",
+                table: "Issue",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Book",
+                table: "Book",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Author",
+                table: "Author",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Genre_GenreId",
+                table: "Book",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issue_AspNetUsers_UserId",
+                table: "Issue",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Issue_Book_BookId",
+                table: "Issue",
+                column: "BookId",
+                principalTable: "Book",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
