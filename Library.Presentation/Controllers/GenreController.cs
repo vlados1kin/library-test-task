997fd1bb526cbc4cbc71499398a4e172c71aa8a0
Vladislav Lashkin <lashkin2004@gmail.com>
2024-09-04 14:20:07 +0300

    fix genre controller

--- Library.Presentation/Controllers/GenreController.cs
+++ Library.Presentation/Controllers/GenreController.cs
@@ -1,4 +1,4 @@
-﻿using Library.Contracts;
+﻿using Library.Service.GenreUseCases;
 using Library.Shared.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,45 +8,58 @@ namespace Library.Presentation.Controllers;
 [Route("api/genres")]
 public class GenreController : ControllerBase
 {
-    private readonly IServiceManager _service;
-
-    public GenreController(IServiceManager service)
+    private readonly GetGenresUseCase _getGenresUseCase;
+    private readonly GetGenreByIdUseCase _getGenreByIdUseCase;
+    private readonly CreateGenreUseCase _createGenreUseCase;
+    private readonly UpdateGenreUseCase _updateGenreUseCase;
+    private readonly DeleteGenreUseCase _deleteGenreUseCase;
+    
+    public GenreController(
+        GetGenresUseCase getGenresUseCase,
+        GetGenreByIdUseCase getGenreByIdUseCase,
+        CreateGenreUseCase createGenreUseCase,
+        UpdateGenreUseCase updateGenreUseCase,
+        DeleteGenreUseCase deleteGenreUseCase)
     {
-        _service = service;
+        _getGenresUseCase = getGenresUseCase;
+        _getGenreByIdUseCase = getGenreByIdUseCase;
+        _createGenreUseCase = createGenreUseCase;
+        _updateGenreUseCase = updateGenreUseCase;
+        _deleteGenreUseCase = deleteGenreUseCase;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetGenres()
     {
-        var genresDto = await _service.GenreService.GetGenresAsync(trackChanges: false);
+        var genresDto = await _getGenresUseCase.ExecuteAsync(trackChanges: false);
         return Ok(genresDto);
     }
     
     [HttpGet("{id:guid}", Name = "GetGenreById")]
     public async Task<IActionResult> GetGenreById(Guid id)
     {
-        var genreDto = await _service.GenreService.GetGenreByIdAsync(id, trackChanges: false);
+        var genreDto = await _getGenreByIdUseCase.ExecuteAsync(id, trackChanges: false);
         return Ok(genreDto);
     }
     
     [HttpPost]
     public async Task<IActionResult> CreateGenre([FromBody] GenreForCreationDto genreForCreationDto)
     {
-        var genreDto = await _service.GenreService.CreateGenreAsync(genreForCreationDto);
+        var genreDto = await _createGenreUseCase.ExecuteAsync(genreForCreationDto);
         return CreatedAtRoute("GetGenreById", new { Id = genreDto.Id }, genreDto);
     }
     
     [HttpPut("{id:guid}")]
     public async Task<IActionResult> UpdateGenre(Guid id, [FromBody] GenreForUpdateDto genreForUpdateDto)
     {
-        await _service.GenreService.UpdateGenreAsync(id, genreForUpdateDto, trackChanges: true);
+        await _updateGenreUseCase.ExecuteAsync(id, genreForUpdateDto, trackChanges: true);
         return NoContent();
     }
     
     [HttpDelete("{id:guid}")]
     public async Task<IActionResult> DeleteGenre(Guid id)
     {
-        await _service.GenreService.DeleteGenreAsync(id, trackChanges: true);
+        await _deleteGenreUseCase.ExecuteAsync(id, trackChanges: true);
         return NoContent();
     }
 }
